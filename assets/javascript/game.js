a474a81a2ecbe43b0ddaed4f9a26fc163c61775d
Dibora D <diborad@yahoo.com>
2019-08-13 13:48:48 -0500

    js onlick event added joe is nice

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,92 +1,51 @@
-$(document).ready(function() {
+const wordList = ['Canada','Algeria','Ethiopia', 'Greece'];
+const hint = ['The 2 main languages spoken in Canada are English and French.','It is the largest country in Africa','Addis Ababa, is the capital city of this country.',
+'The offcial name of this country is Hellenic Republic.'];
 
-      
-    var letter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
- // Create an array of words
-var game = [
-    "javascript",
-    "monkey",
-    "amazing",
-    "pancake",
-    
-    ];
-    /*
-    var choice = words[Math.floor(Math.random() * 4)];
-    var answer = game [choice];
-    var myLength = answer.length;
-    var display=[myLength];
-    var win = myLength;
-    var attemptsLeft = 10;
-    var output="";
-    var userLetter="";
-
-    var setup = function(){
-        for (var i=0; i<answer.length;i++){
-            display[i] = " _ ";
-            output = output + display[i];
-        }
-        document.getElementById("game").innerHTML = output;
-        output ="";
-    }
- var submit = function(){
-     output = "";
-     userLetter=$("letter").value;
-     $("letter").value ="";
-
-     for (var c=0; c<answer.length; c++){
-         alert(letters[c]);
-     }
- }
-
-    */
-    for (var i = 0; i < letters.length; i++) {
-     if(answer == guess){
-         alert("you Guessed Correctly!");
-         break;
-     }else{
-         guess = prompt("Sorry Try Again");
-     }
-
-
-
-      var letterBtn = $("<button>");
-
-      
-      letterBtn.addClass("letter-button letter letter-button-color");
-
-     
-      letterBtn.attr("data-letter", letters[i]);
-
-     
-      letterBtn.text(letters[i]);
-
-      
-      $("#buttons").append(letterBtn);
-
-    }
-
-    
-    $(".letter-button").on("click", function() {
-
-     
-      var lettersGuess = $("<div>");
-
-     
-      lettersGuess.addClass("letter lettersGuess-color");
-
-      lettersGuess.text($(this).attr("data-letter"));
-
-      
-      $("#display").append(lettersGuess);
-
-    });
-
-    
-    $("#clear").on("click", function() {
-
-      $("#display").empty();
-
-    });
-
-  });
+window.onload = function () {
   
+  document.getElementById('currentWord').onclick = printCurrentWord;
+}
+
+function printCurrentWord (){
+   document.getElementById('currentWord').innerText = "Joe is nice";
+}    
+
+
+// const gameCode = {
+//   Wins = 0,
+//   lives = 10,
+//   currentWord: [],
+//   currentWord : [],
+//   lettersGuessedWrong: [],
+//   hint :[],
+
+// }
+
+  function getRandomNumber() {
+    return Math.floor(Math.random() * z);
+  };
+
+//   function getNewWord() {
+//      const z = this.getRandomNumber(wordsToGuessFrom.length);
+//      this.currentWord.push(wordsToGuessFrom[z]);
+//      this.factOfCurrentWord.push(facts[z]);
+//      for (let i = 0; i < this.currentWord[0].length; i++) {
+//        this.currentWord.push('-');
+//       }
+//     };
+
+//  function(guessLetterchar) {
+//   if (this.currentWord[0].includes(char.toLowerCase())) {
+//       for (let i = 0; i < this.currentWord[0].length; i++) {
+//           if (this.currentWord[0][i]==char) {
+//               this.currentWordPublic[i] = char;
+//           }
+//       }
+//   } else if (this.lettersGuessedWrong.includes(char)) {
+//       console.log('Already guessed that letter, you have.');
+//   } else {
+//       this.lettersGuessedWrong.push(char);
+//       this.livesLeft--;
+//   }
+// };
