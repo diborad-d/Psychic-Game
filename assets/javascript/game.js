8c78c8d541f452001613e5e15b5438b7d504117f
Dibora D <diborad@yahoo.com>
2019-08-14 15:14:37 -0500

    current word and hint to generate together

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 const wordList = ['Canada','Algeria','Ethiopia', 'Greece'];
-const hintList = ['The 2 main languages spoken in Canada are English and French.','It is the largest country in Africa','Addis Ababa, is the capital city of this country.',
+const hintList = ['The 2 main languages spoken in this country are English and French.','It is the largest country in Africa','Addis Ababa, is the capital city of this country.',
 'The official name of this country is Hellenic Republic.'];
 
 window.onload = function () {
@@ -7,12 +7,14 @@ window.onload = function () {
 }
 
 function setup() {
-    document.getElementById("currentWord").innerText = "Current Word: " + wordList[currentIndex()];
+  var currentIndex = getRandomIndex();
+
+  document.getElementById("currentWord").innerText = "Current Word: " + wordList[currentIndex];
+  document.getElementById("hint").innerText = "Hint:" + hintList[currentIndex];
 }
 
-function currentIndex() { 
+function getRandomIndex() { 
   return Math.floor(Math.random() * Math.floor(wordList.length-1));
 }
-
 
 
